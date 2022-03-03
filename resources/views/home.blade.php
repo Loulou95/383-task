8409f04a523dfd55bed1b7987347077919cd565b
DESKTOP-MH088Q2\Lenovo 2020 <louisversace@yahoo.co.uk>
2022-03-03 11:41:57 +0000

    home view updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,57 +1,13 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="{{ asset('js/weather.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <style>
-
-    </style>
-</head>
-<body>
-    <div class="wrapper">
-        <div class="content">Hidden content</div>
-        <div class="toggle">Click</div>
+            </div>
+        </div>
     </div>
-
-    <div class="wrapper">
-        <div class="content">Hidden content</div>
-        <div class="toggle">Click</div>
-    </div>
-
-    <div class="wrapper">
-        <div class="content">Hidden content</div>
-        <div class="toggle">Click</div>
-    </div>
-
-    <div class="wrapper">
-        <div class="content">Hidden content</div>
-        <div class="toggle">Click</div>
-    </div>
-
-    <div class="wrapper">
-        <div class="content">Hidden content</div>
-        <div class="toggle">Click</div>
-    </div>
-
-    <div class="wrapper">
-        <div class="content">Hidden content</div>
-        <div class="toggle">Click</div>
-    </div>
-
-</body>
+</div>
+@endsection
